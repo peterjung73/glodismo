3057f0d4e546cec45c687e2f6858d390c1eec62c
Peter Jung <peter.jung@tu-berlin.de>
2022-09-07 15:01:58 +0200

    now

--- plots.py
+++ plots.py
@@ -17,7 +17,7 @@ def plot(ax, df, label, key, color):
     ax.plot(means, label=label)
     ax.fill_between(range(len(means)), means - stds, means + stds, alpha=0.3, linewidth=0.0)
 
-def plot_average(algs,key,experiment,resultsdir='results',traintest='train'):
+def plot_average(algs,key,experiment,resultsdir='results',traintest='train',suffix='Synthetic*_*'):
     fig_dpi=96
     fig_ext='png'
     fig_dir=resultsdir
@@ -26,7 +26,7 @@ def plot_average(algs,key,experiment,resultsdir='results',traintest='train'):
     for alg in algs:
         df=[]
         csvfiles=glob.glob(os.path.join(resultsdir+'/'+ 
-            experiment+'_'+alg+'_Synthetic*_*_'+traintest+'.csv'))
+            experiment+'_'+alg+'_'+suffix+'_'+traintest+'.csv'))
         for csvfile in csvfiles:    
             df.append(pd.read_csv(csvfile))
         plot(axes1,df,alg,key,"Red")
@@ -37,7 +37,7 @@ def plot_average(algs,key,experiment,resultsdir='results',traintest='train'):
     plt.savefig(fig_dir+'/average_'+experiment+'_'+key+'.'+fig_ext,dpi=fig_dpi,bbox_inches='tight')
 
 
-def plot_history(algs,key,experiment,resultsdir='results',traintest='train'):
+def plot_history(algs,key,experiment,resultsdir='results',traintest='train',suffix='Synthetic*_*'):
     fig_dpi=96
     fig_ext='png'
     fig_dir=resultsdir
@@ -45,7 +45,7 @@ def plot_history(algs,key,experiment,resultsdir='results',traintest='train'):
     axes1 = fig.add_subplot(1, 1, 1)
     for alg in algs:
         msft = pd.concat(map(pd.read_csv, glob.glob(os.path.join(resultsdir+'/'+ 
-            experiment+'_'+alg+'_Synthetic*_*_'+traintest+'.csv'))))
+            experiment+'_'+alg+'_'+suffix+'_'+traintest+'.csv'))))
         axes1.plot(msft[key].values,label=alg)
     axes1.set_ylabel(key)
     axes1.legend()
